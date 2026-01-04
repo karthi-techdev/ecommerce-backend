243e4ff7ee67579d6b05bc509cc5706509f661c2
Karthi <138133344+karthi-techdev@users.noreply.github.com>
2026-01-04 19:20:50 +0530

    Update MongoDB URI and fix formatting

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,7 +7,7 @@ function getEnv(variable: string, fallback: string): string {
 }
 
 const isProduction = process.env.NODE_ENV === 'production';
-const fallbackUri = "mongodb+srv://kr748283_db_user:goofQ5Dl10usXX4k@baz-cluster.kosdkgc.mongodb.net/baz-cluster";
+const fallbackUri = "mongodb://localhost:27017/ecommerce";
 const MONGO_URI = isProduction ? getEnv("MONGO_URI", fallbackUri) : getEnv("LOCAL_MONGO_URI", fallbackUri);
 
 export const ENV = {
@@ -16,4 +16,5 @@ export const ENV = {
   ADMIN_EMAIL: getEnv("ADMIN_EMAIL", ""),
   JWT_SECRET: getEnv("JWT_SECRET", ""),
   JWT_EXPIRE_TIME: getEnv("JWT_EXPIRE_TIME", "")
+
 }
