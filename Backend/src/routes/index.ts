25efd0ee54deca8bafa9f744b32f9cac9321222b
ashmitha-30 <ashmithaashu2004@gmail.com>
2026-04-28 22:14:37 +0530

    order-management

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -22,20 +22,15 @@ import sliderRoutes from './sliderRoutes'
 import blogRoutes from "./blogRoutes";
 import settingsRoutes from "./settingsRoutes";
 import offerRoutes from "./offerRoutes";
-
 import commentsRoutes from "./commentsRoutes";
-
 import reviewRoutes from "./reviewRoutes";
 import usersRoutes from "./usersRoutes";
-
-
-
-
 import subscribeRoutes from "./subscribeRoutes"
-
 import contactRoutes from "./contactRoutes"
 import addInfoRoutes from './addInfoRoutes'
 import bannerRoutes from "./bannerRoutes";
+
+import accountRoutes from "./accountRoutes";
 
 export default function registerRoutes(app: Express) {
   app.use("/api/v1/auth", authenticationRoutes);
@@ -64,12 +59,12 @@ export default function registerRoutes(app: Express) {
   app.use("/api/v1/admin/banner", bannerRoutes);
   app.use("/api/v1/admin/comments", commentsRoutes);
 
-
-  app.use("/api/users", usersRoutes);
   app.use("/api/v1/admin/banner", bannerRoutes);
   app.use("/api/v1/admin/subscribe", subscribeRoutes);
   app.use("/api/v1/admin/contact", contactRoutes);
   app.use("/api/v1/admin/product/add-info", addInfoRoutes);
   app.use("/api/users", usersRoutes);
+
+  app.use("/api/v1/admin/account", accountRoutes);
 
 }
--- Backend/src/routes/usersRoutes.ts
+++ Backend/src/routes/usersRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { register, login } from "../controllers/usersController";
+import { register, login,updateProfile, changePassword} from "../controllers/usersController";
+import { protectAdmin } from "../middleware/adminAuthMiddleware";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 
+router.put("/update-profile", protectAdmin, updateProfile);
+
+router.post("/change-password", protectAdmin, changePassword);
 export default router;
